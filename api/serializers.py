28d3648f02927900b1a91522fe16928070261695
vish-2604 <shahvish26@gmail.com>
2025-03-05 18:52:07 +0530

    removed api app

--- api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from adminside.models import Table
-
-class TableSerializer(serializers.ModelSerializer):
-    table_id = serializers.IntegerField(read_only=True)  # Ensure auto-increment behavior
-
-    class Meta:
-        model = Table
-        fields = ['table_id', 'seats', 'status']  # Include table_id, but it's read-only
